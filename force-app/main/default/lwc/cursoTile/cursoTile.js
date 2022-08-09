50ff48b8f4b700dc0bcb84417ec1acb43fc8c34f
jaikipa <jcayquipa@gmail.com>
2022-08-09 05:01:47 -0500

    Diseño de grid para front, con redirección JS Detalle

--- force-app/main/default/lwc/cursoTile/cursoTile.js
+++ force-app/main/default/lwc/cursoTile/cursoTile.js
@@ -1,6 +1,32 @@
 import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class CursoTile extends LightningElement {
+
+export default class CursoTile extends NavigationMixin(LightningElement) {
     @api curso;
 
+    //curso.Location__c = "Online";
+    //curso.Location__c = "Grabado";
+    //curso.Location__c = "Online, Grabado";
+
+    get isOnline(){
+        return this.curso.Location__c.includes("Online");
+    }
+
+    get isGrabado(){
+        return this.curso.Location__c.includes("Grabado");
+    }
+
+
+    navigateToRecordViewPage() {
+        // Navigate to the View PAge 
+        this[NavigationMixin.Navigate]({
+            type: 'standard__RecordPage',
+            attributes: {
+                RecordId: this.curso.Id,
+                objectApiName: 'Curso__c', //objectApiName is optional
+                actionName: 'view',
+            },
+        });
+    }
 }
