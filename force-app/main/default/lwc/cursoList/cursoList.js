556e18a29eddfaf173428377adcb45815b10ea5a
jaikipa <jcayquipa@gmail.com>
2022-08-09 03:18:23 -0500

    creación de contenido de pruebas

---
+++ force-app/main/default/lwc/cursoList/cursoList.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+
+export default class CursoList extends LightningElement {
+    curso = [
+        {
+            Name: "Matematicas 03 ",
+            Description__c: "Curso de mátematicas intermedio",
+            Image_URL__c: 
+            "https://1.bp.blogspot.com/-MBHX6dMn7v4/ViE8NhafueI/AAAAAAAAMlo/P_q9A9a-DPU/s1600/8%2Bcursos%2Bgratis%2Bmatematicas%2Blogica.jpg",
+            Location__c: "Grabado,Online"
+        },
+        {
+            Name: "Matematicas 04 ",
+            Description__c: "Curso de mátematicas Avanado",
+            Image_URL__c: 
+            "https://1.bp.blogspot.com/-MBHX6dMn7v4/ViE8NhafueI/AAAAAAAAMlo/P_q9A9a-DPU/s1600/8%2Bcursos%2Bgratis%2Bmatematicas%2Blogica.jpg",
+            Location__c: "Grabado,Online"
+        },
+        {
+            Name: "Matematicas 05 ",
+            Description__c: "Curso de mátematicas basico",
+            Image_URL__c: 
+            "https://1.bp.blogspot.com/-MBHX6dMn7v4/ViE8NhafueI/AAAAAAAAMlo/P_q9A9a-DPU/s1600/8%2Bcursos%2Bgratis%2Bmatematicas%2Blogica.jpg",
+            Location__c: "Grabado,Online"
+        },
+    ];
+}
--- force-app/main/default/lwc/cursoTile/cursoTile.js
+++ force-app/main/default/lwc/cursoTile/cursoTile.js
@@ -1,11 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 export default class CursoTile extends LightningElement {
-
-    curso = {
-        Name: "Matematicas 03 ",
-        Description__c: "Curso de mátematicas intermedio",
-        Image_URL__c: 
-        "https://1.bp.blogspot.com/-MBHX6dMn7v4/ViE8NhafueI/AAAAAAAAMlo/P_q9A9a-DPU/s1600/8%2Bcursos%2Bgratis%2Bmatematicas%2Blogica.jpg",
-        Location__c: "Grabado,Online"
-    };
+    @api curso;
 }
